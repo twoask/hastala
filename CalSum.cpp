e3b2ff26e10e8b7d88280a76417b5136d22d1d7d
Hai Vu <twoask@gmail.com>
2015-01-26 23:12:41 -0800

    Provided a simple unit test case for CalSum

--- CalSum.cpp
+++ CalSum.cpp
@@ -10,15 +10,28 @@
  * Signature: long CalSum(int N)
  */
 #include <iostream>
+#include <cassert>
 
 long CalSum(int N)
 {
     // TODO<hieuvu96>: need to work on this
-	return 1; // <haitv>: Not this not correct
+	return 0; // <haitv>: Not this not correct
+}
+
+/*
+ * Unit test function for CalSum.
+ */
+void CalSumTest()
+{
+    assert(CalSum(0) == 0);
+    assert(CalSum(1) == 1);
+    assert(CalSum(4) == 10);
 }
 
 int main()
 {
+    // Do Unit Test
+    CalSumTest();
     // TODO<hieuvu96>: do more testings
 	std::cout << "Return: " << CalSum(1) << std::endl;
     return 0;
