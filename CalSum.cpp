31b98a41cc9beda75677226bd172be9772a31e83
Hai Vu <twoask@gmail.com>
2015-01-26 22:39:05 -0800

    Initial requirements for CalSum.cpp

---
+++ CalSum.cpp
@@ -0,0 +1,17 @@
+/*
+ * CalSum.cpp
+ *
+ * Calculate the sum of all number from 1 to N (given N).
+ *
+ * Signature: long CalSum(int N)
+ */
+long CalSum(int N)
+{
+    // TODO: fill in the code here
+}
+
+int main()
+{
+    // TODO: write your tests here
+    return 0;
+}
